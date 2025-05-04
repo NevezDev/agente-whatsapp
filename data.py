7cd5eb3c36fcff2bf23822d9f6ea3df865bfcd54
NevezDev <serginho-nvs@hotmail.com>
2025-05-04 17:50:38 -0300

    alterando...

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-products = [
+produtos = [
     {"nome": "Beijinho", "categoria": "Doces", "descricao": "Docinho de coco com leite condensado", "preco": 1.50},
     {"nome": "Brigadeiro", "categoria": "Doces", "descricao": "Tradicional docinho de chocolate", "preco": 1.50},
     {"nome": "Bolo de Chocolate", "categoria": "Bolos", "descricao": "Bolo fofinho com cobertura de chocolate", "preco": 25.00},
